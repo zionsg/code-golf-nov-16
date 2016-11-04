dbd8435655919f6f8d711377fd70deb305ddbfd1
Ng Zhi An <ngzhian@gmail.com>
2016-11-04 19:21:06 +0800

    add

---
+++ question1/me.py
@@ -0,0 +1,9 @@
+def z(a):
+    c = ""
+    r=range
+    l=len
+    for i in r(len(a)):
+        b = a[i:]
+        c += (''.join(b[i] * (i+1) for i in range(len(b)))) + "\n"
+    return c
+print(z("asdf"))
