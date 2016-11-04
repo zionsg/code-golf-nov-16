51f0e5132f9ac269a347b15330710d819a209190
Ng Zhi An <ngzhian@gmail.com>
2016-11-04 19:56:58 +0800

    better

--- question1/2.py
+++ question1/2.py
@@ -1,5 +1,1 @@
-
-def z(a):
-    b = [1] + a
-    c = a[1:] + [1]
-    return max(a[i] for i in range(len(a)) if c[i] == 0 or b[i] == 0)
+def z(a): return max(a[i] for i in range(len(a)) if (a[1:]+[1])[i] == 0 or ([1]+a)[i] == 0)
