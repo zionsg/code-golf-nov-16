13e24a0d2a72b60d2ea094572a348bae9772e520
Eugene Yeo <yeo.eugene.oey@gmail.com>
2016-11-04 21:00:38 +0800

    Fix

--- question4/eugene.py
+++ question4/eugene.py
@@ -1,8 +1,8 @@
 def f(s,k):
+    s+= " "*(len(s)%k)
     r = ''
     for i in range(k):
-        _ = ' ' if i % k else ''
-        r += s[i::k] + _
+        r += s[i::k]
     return r
 
 print(f('codegolf.stackexchange.com', 4))
